e2541daa3a0e0a19e4dea73e51383e5b9c39586d
Amanda Hansson <amanda9307@gmail.com>
2025-01-07 12:23:33 +0100

    :bug: Fixed refresh theme toggle

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,14 +3,10 @@ import { Outlet } from "react-router";
 export const Layout = () => {
   const theme = localStorage.getItem("theme");
   const body = document.querySelector("#body");
-  if (theme == "light") {
-    body?.classList.remove("light");
+  if (theme == "dark") {
     body?.classList.add("dark");
-    localStorage.setItem("theme", "dark");
   } else {
-    body?.classList.remove("dark");
     body?.classList.add("light");
-    localStorage.setItem("theme", "light");
   }
   return (
     <>
