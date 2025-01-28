b61b41dd9c9143682fa2e22e1b3152daae122cfe
Amanda Hansson <amanda9307@gmail.com>
2025-01-28 11:04:26 +0100

    Updates

---
+++ assets/Button-BwEmg0zX.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-t8r2_LHW.js";const a=({children:t,onClick:s,bgColor:o})=>r.jsx("button",{className:o,onClick:s,children:t});export{a as B};
--- assets/Button-Cq6hTME8.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DBiGbhcg.js";const a=({children:t,onClick:s,bgColor:o})=>r.jsx("button",{className:o,onClick:s,children:t});export{a as B};
